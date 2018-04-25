725fcb6f2399ea6c96443efebf64238dc36ac6c6
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-04-25 12:54:15 -0500

    initial commit

---
+++ DialogueDeserial/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace DialogueDeserial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var path = System.IO.Path.Combine(Environment.CurrentDirectory, "Dialogue.json");
+            var data = File.ReadAllText(path);
+            DialogueNode node = new DialogueNode();
+            var dialogueTree = JsonConvert.DeserializeObject<DialogueTree>(data);
+            dataGridView1.ScrollBars = ScrollBars.Both;
+            //dataGridView1.Rows[0].Cells[0] = data[0]
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DialogueDeserial/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogueDeserial
+{
+    public class DialogueTree
+    {
+        public List<DialogueRoot> DialogueRoots;
+    }
+
+    public class DialogueRoot
+    {
+        public List<DialogueNode> DialogueNodes;
+    }
+
+    public class DialogueNode
+    {
+        public string ConversationID;
+        public string ParticipantName;
+        public string EmoteType;
+        public string Side;
+        public string Line;
+        public string SpecialtyAnimation;
+        public string SpecialtyCamera;
+        public string Participants;
+        public string ConversationSummery;
+
+    }
+    static class Program
+    {
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
